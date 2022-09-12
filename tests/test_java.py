88aacc9e5375da56fe3deb298195398a39fd9404
Dan Nicholson <dbn@endlessos.org>
2022-09-12 10:39:00 -0600

    More pytest work

--- tests/test_java.py
+++ tests/test_java.py
@@ -1,5 +1,6 @@
 import pytest
 import sys
+from pythonforandroid.recipe import Recipe
 from pythonforandroid.toolchain import ToolchainCL
 
 
@@ -8,15 +9,16 @@ def storage_dir(tmp_path_factory):
     return tmp_path_factory.mktemp('storage')
 
 
-def test_python_util(storage_dir, tmp_path, monkeypatch):
+@pytest.fixture
+def src_dir(tmp_path):
     src_dir = tmp_path / 'src'
     src_dir.mkdir()
-    entrypoint = src_dir / 'main.py'
-    entrypoint.touch()
+    return src_dir
 
-    argv = [
-        'toolchain.py',
-        'test',
+
+@pytest.fixture
+def test_options(storage_dir, src_dir):
+    return [
         '--package=org.example.Test',
         '--version=0.1',
         '--name=test',
@@ -31,6 +33,36 @@ def test_python_util(storage_dir, tmp_path, monkeypatch):
             'pyjnius,android'
         ),
     ]
+
+
+@pytest.fixture
+def unit_test_argv(test_options, monkeypatch):
+    argv = ['toolchain.py', 'test'] + test_options
     monkeypatch.setattr(sys, 'argv', argv)
 
+
+@pytest.fixture
+def connected_test_argv(test_options, monkeypatch):
+    argv = ['toolchain.py', 'connectedAndroidTest', '--enable-androidx'] + test_options
+    monkeypatch.setattr(sys, 'argv', argv)
+
+
+@pytest.fixture
+def cleanup_recipes():
+    yield
+    if hasattr(Recipe, 'recipes'):
+        del Recipe.recipes
+
+
+def test_android_unit_tests(src_dir, unit_test_argv, cleanup_recipes):
+    """Android unit tests"""
+    entrypoint = src_dir / 'main.py'
+    entrypoint.touch()
+    ToolchainCL()
+
+
+def test_android_instrumented_tests(src_dir, connected_test_argv, cleanup_recipes):
+    """Android instrumented tests"""
+    entrypoint = src_dir / 'main.py'
+    entrypoint.touch()
     ToolchainCL()
